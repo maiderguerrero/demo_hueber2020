f65497a8d4808200049fdaad6c0f327920143217
maiderguerrero <47318536+maiderguerrero@users.noreply.github.com>
2020-03-05 09:14:07 +0100

    Update style.js

--- style.js
+++ style.js
@@ -44,9 +44,11 @@
 
 				{ name: 'Tabla 1', element: 'table', type: 'bck-stack-class', attributes: { 'class': 'bck-table-1'} },
 				{ name: 'Tabla 2', element: 'table', type: 'bck-stack-class', attributes: { 'class': 'bck-table-2'} },
+				{ name: 'Tabla 3', element: 'table', type: 'bck-stack-class', attributes: { 'class': 'bck-table-3'} },
 				{ name: 'Celda 1', element: 'td', attributes: { 'class': 'bck-td-1'} },
 				{ name: 'Celda 2', element: 'td', attributes: { 'class': 'bck-td-2'} },
 				{ name: 'Celda 3', element: 'td', attributes: { 'class': 'bck-td-3'} },
+				{ name: 'Celda 4', element: 'td', attributes: { 'class': 'bck-td-4'} },
 
 				{ name: 'Presentación', element: 'h4', attributes: { 'class': 'presentation'} },
 				{ name: 'Presentación 2', element: 'h4', attributes: { 'class': 'presentation-2'} },
